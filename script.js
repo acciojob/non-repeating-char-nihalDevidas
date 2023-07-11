96d2a837f2cb2ed3308df2c46b6f2946a7907fcc
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-07-11 23:47:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let s = prompt();
+let o = {};
+for(let i=0; i< s.length; i++){
+  
+  if(o[s[i]]){      
+    o[s[i]]++;
+  }
+  else{
+    o[s[i]] = 1;
+  }
+}
+
+for(let i=0; i< s.length; i++){
+  if(o[s[i]] === 1){
+    alert(s[i]);
+    break;
+  }
+}
